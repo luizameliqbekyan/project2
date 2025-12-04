e65b694b141274e206296e08dec39ef754b4f185
Luiza <luiza.meliqbekyan@avromic.com>
2025-12-04 19:17:21 +0400

    changes

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo "hello world";
+    echo "change";
+
+    ?>
+</body>
+</html>
+
